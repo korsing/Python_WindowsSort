106f83e0c6e76f3c33f8a3b4f5076f01df11a781
DESKTOP-4SHJTLB\Joshua Y. S. Jung <joshuanpedia@gmail.com>
2018-03-14 16:31:37 +0900

    02분반 제출자, 중복제출자, 미제출자 구분 기능 추가

--- Sort_files_Windows(02).py
+++ Sort_files_Windows(02).py
@@ -24,60 +24,79 @@ def convert_original():
         if(student_id == None or division == None):
             continue
 
-        # 분반 내역으로 
+        # 분반 정보를 리스트로 저장
         if(division == 32423):
             division_32423.append(student_id)
-            print("{}가 32423으로 분류되었습니다.".format(student_id))
         elif(division == 32530):
             division_32530.append(student_id)
-            print("{}가 32530으로 분류되었습니다.".format(student_id))
         elif(division == 32531):
             division_32531.append(student_id)
-            print("{}가 32531으로 분류되었습니다.".format(student_id))
         elif(division == 50313):
             division_50313.append(student_id)
-            print("{}가 50313으로 분류되었습니다.".format(student_id))
         else:
-            print("일단 오류...")
+            print("{}의 분류 정보가 없습니다. 학번을 확인해주세요. ".format(student_id))
+        student_list = division_32423 + division_32530 + division_32531 + division_50313
 
-def distribute():
+    return distribute(student_list)
+    
+def distribute(student_list):
     os.chdir("C:\\Users\\Joshua Y. S. Jung\\Desktop\\icampus\\Original")
-    total_list = os.listdir()
+    # 각각 분류할 폴더를 생성
     os.mkdir("C:\\Users\\Joshua Y. S. Jung\\Desktop\\icampus\\32423")
     os.mkdir("C:\\Users\\Joshua Y. S. Jung\\Desktop\\icampus\\32530")
     os.mkdir("C:\\Users\\Joshua Y. S. Jung\\Desktop\\icampus\\32531")
     os.mkdir("C:\\Users\\Joshua Y. S. Jung\\Desktop\\icampus\\50313")
-    count = [0,0,0,0,0]
+    
+    # 총 제출 인원 및 분반별 제출 인원을 확인하기 위한 변수 선언
+    count = [0,0,0,0,0,0]
+    
+    # Original 폴더에 있는 전체 목록을 하나씩 traversing 하면서 분류 시작
+    total_list = os.listdir()
+    done = []
     for file in total_list:
+        # 일단 그 파일이 무엇인지 출력
         print(file)
+        # 더러운 파일 앞 10글자는 학번.. 그 부분만 슬라이싱해서 비교하기 위해 사용
         if(int(file[:10]) in division_32423):
             shutil.copy(file, "C:\\Users\\Joshua Y. S. Jung\\Desktop\\icampus\\32423")
-            print("{}가 분류되었습니다.".format(file))
             count[1] += 1
         elif(int(file[:10]) in division_32530):
             shutil.copy(file, "C:\\Users\\Joshua Y. S. Jung\\Desktop\\icampus\\32530")
-            print("{}가 분류되었습니다.".format(file))
             count[2] += 1
         elif(int(file[:10]) in division_32531):
             shutil.copy(file, "C:\\Users\\Joshua Y. S. Jung\\Desktop\\icampus\\32531")
-            print("{}가 분류되었습니다.".format(file))
             count[3] += 1
         elif(int(file[:10]) in division_50313):
             shutil.copy(file, "C:\\Users\\Joshua Y. S. Jung\\Desktop\\icampus\\50313")
-            print("{}가 분류되었습니다.".format(file))
+            
             count[4] += 1
         else:
-            print("{} 오류..".format(file))
-
+            print("{}의 분류 정보가 없습니다. 학번을 확인해주세요. ".format(file[:10]))
+        
+        if(int(file[:10]) in done):
+            # 여러파일을 제출한 학생 카운터 증가
+            count[5] += 1            
+        else:
+            done.append(int(file[:10]))
+            del student_list[student_list.index(int(file[:10]))]
+        
+        # 4개 분반의 제출 수를 더해서 총 제출수를 확인
         count[0] = count[1]+count[2]+count[3]+count[4]
-    return count
+    return (count, student_list)
     
-def end(count):
-    print("총 제출 {}명 중 {}명".format(165, count[0]))
-    print("32423 : {}명".format(count[1]))
-    print("32530 : {}명".format(count[2]))
-    print("32531 : {}명".format(count[3]))
-    print("50313 : {}명".format(count[4]))
+def end(count, not_handin):
+    print("총 {}개 과제 제출".format(count[0]))
+    print("총 {}명 중 {}명 제출 {}명 미제출".format(165, count[0] - count[5], 165 - (count[0] - count[5])))
+
+    print("미제출자 : ", end="")
+    for student in not_handin:
+        print("{} ".format(student), end="")
+    print("")
+    
+    print("경제관 32423호 : {:3}개".format(count[1]))
+    print("경제관 32530호 : {:3}개".format(count[2]))
+    print("경제관 32531호 : {:3}개".format(count[3]))
+    print("호암관 50313호 : {:3}개".format(count[4]))
 
 # 구분할 빈 리스트 생성
 division_32423 = []
@@ -86,11 +105,10 @@ division_32531 = []
 division_50313 = []
 
 # 전체 리스트에서 분반 리스트 생성
-convert_original()
+
+count = convert_original()
 print("\n\n\n")
-count = distribute()
-print("\n\n\n")
-end(count)
+end(count[0], count[1])
 
 
 
